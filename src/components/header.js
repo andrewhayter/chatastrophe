0cb107114c41e82e5851e5e88c80a93b9f935f3e
Andrew Hayter <andrewhayter@gmail.com>
2017-09-08 20:47:47 -0700

    breaks up LoginContainer component and adds firebase

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'React';
+
+const Header = () => {
+  return (
+    <div id="Header">
+      <img src="/assets/icon.png" alt="logo" />
+      <h1>Chatastrophe</h1>
+    </div>
+  )
+}
+
+export default Header;
